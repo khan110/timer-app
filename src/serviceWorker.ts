2179abb38283d715a9f3f7482cdbf914649f29f0
Umair ali khan <khan110@users.noreply.github.com>
2020-09-07 01:07:23 -0500

    update
    make pwa with cached pages

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -147,3 +147,19 @@ export function unregister() {
       });
   }
 }
+
+var CACHE_NAME = 'my-pwa-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/styles.css',
+  '/script/webpack-bundle.js'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        // Open a cache and cache our files
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
